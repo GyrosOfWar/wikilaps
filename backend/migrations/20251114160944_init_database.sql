4692ae8727fd71713e6d94893a39c2001089a42e
Martin Tomasi <martin.tomasi@gmail.com>
2025-11-14 17:48:24 +0100

    start adding some database migrations

---
+++ backend/migrations/20251114160944_init_database.sql
@@ -0,0 +1,26 @@
+CREATE TABLE race_weekend (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    year INT NOT NULL,
+    location VARCHAR NOT NULL,
+    circuit_name VARCHAR NOT NULL,
+    country_key VARCHAR NOT NULL
+);
+
+CREATE TYPE session_type AS ENUM ('FreePractice', 'SprintQualification', 'SprintRace', 'Qualifying', 'Race');
+
+CREATE TABLE session (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    weekend_id BIGINT REFERENCES race_weekend (id) NOT NULL,
+    session_type session_type NOT NULL,
+    start_time TIMESTAMPTZ NOT NULL,
+    end_time TIMESTAMPTZ
+);
+
+CREATE TYPE vote_type AS ENUM ('FullRace', 'RaceIn30', 'Highlights');
+
+CREATE TABLE votes (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    vote_type vote_type NOT NULL,
+    user_identifier VARCHAR NOT NULL,
+    session_id BIGINT REFERENCES session (id) NOT NULL
+);
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use dotenvy::dotenv;
+use sqlx::PgPool;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
+pub type Result<T> = color_eyre::Result<T>;
+
+pub struct AppConfig {
+    pub database_url: String,
+}
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db: Database,
+}
+
+#[derive(Clone)]
+pub struct Database {
+    db: PgPool,
+}
+
+impl Database {
+    pub async fn new(db_url: &str) -> Result<Self> {
+        let db = PgPool::connect(db_url).await?;
+        sqlx::migrate!("./migrations").run(&db).await?;
+
+        Ok(Self { db })
+    }
+
+    pub async fn list_weekends(&self) -> Result<()> {
+        let data = sqlx::query!("SELECT * FROM race_weekend")
+            .fetch_all(&self.db)
+            .await?;
+        Ok(())
+    }
+}
+
+async fn hello_world() -> &'static str {
+    "hello, world!"
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let _ = dotenv();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::new("info"))
+        .init();
+    let db_url = std::env::var("DATABASE_URL").expect("Missing DATABASE_URL env var");
+    let database = Database::new(&db_url).await?;
+
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .with_state(AppState { db: database });
+
+    info!("Starting on port 13252");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:13252")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
