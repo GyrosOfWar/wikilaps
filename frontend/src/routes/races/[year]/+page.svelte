52934f0979e202d77d5759480f7df51adb03a627
Martin Tomasi <martin.tomasi@gmail.com>
2026-07-11 08:11:57 +0200

    add a control to jump to races

--- frontend/src/routes/races/[year]/+page.svelte
+++ frontend/src/routes/races/[year]/+page.svelte
@@ -10,15 +10,31 @@
   <h1 class="h2 font-bold tracking-tight">
     {m.weekend_page_heading({ year: data.year })}
   </h1>
-</div>
 
-<section class="grid gap-6 grid-cols-1 mt-8 max-w-3xl self-center w-full">
-  {#each data.weekends as weekend (weekend.id)}
-    <RaceWeekendCard
-      {weekend}
-      class="preset-filled-surface-50-950 border border-surface-200-800 shadow-xl"
-      votes={data.votes}
-      onSubmitVote={submitVote}
-    />
-  {/each}
-</section>
+  <h3 class="text-xs font-bold tracking-wide uppercase opacity-70 mt-4 mb-1">Jump to</h3>
+  <ul class="flex gap-2 w-full flex-wrap">
+    {#each data.weekends as weekend (weekend.id)}
+      <li>
+        <a
+          href={`#round-${weekend.round}`}
+          class="badge preset-filled-secondary-500 shrink-0 font-bold"
+        >
+          <span class="fi fi-{weekend.countryKey.toLowerCase()} shrink-0 rounded-sm shadow-sm"
+          ></span>
+          R{weekend.round}
+        </a>
+      </li>
+    {/each}
+  </ul>
+
+  <section class="grid gap-6 grid-cols-1 mt-8 w-full">
+    {#each data.weekends as weekend (weekend.id)}
+      <RaceWeekendCard
+        {weekend}
+        class="preset-filled-surface-50-950 border border-surface-200-800 shadow-xl"
+        votes={data.votes}
+        onSubmitVote={submitVote}
+      />
+    {/each}
+  </section>
+</div>
